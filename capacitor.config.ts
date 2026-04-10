26318730699a6ff7f6ae303185c75a09ec021329
nivaldosilva0106-ship-it <nivaldosilva0106@gmail.com>
2026-04-10 08:35:17 -0300

    Feat: Atualiza Favicon original para APK, corrige tela branca de incio e otimiza velocidade em celulares fracos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,18 @@ const config: CapacitorConfig = {
     webDir: 'dist',
     server: {
         androidScheme: 'https'
+    },
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 2000,
+            launchAutoHide: true,
+            backgroundColor: "#0a0a0a",
+            androidSplashResourceName: "splash",
+            androidScaleType: "CENTER_CROP",
+            showSpinner: true,
+            androidSpinnerStyle: "large",
+            spinnerColor: "#10b981"
+        }
     }
 };
 
