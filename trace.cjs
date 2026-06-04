a15daf6389329ccca079807ca33d8a859ac8f549
nivaldosilva0106-ship-it <nivaldosilva0106@gmail.com>
2026-06-04 22:48:21 +0100

    feat: implement new UI pages and update APK extraction and configuration scripts

--- trace.cjs
+++
@@ -1,9 +0,0 @@
-const sm = require('source-map');
-const fs = require('fs');
-async function run() {
-  const map = JSON.parse(fs.readFileSync('vercel_index.map', 'utf8'));
-  const consumer = await new sm.SourceMapConsumer(map);
-  const pos = consumer.originalPositionFor({line: 24, column: 8208});
-  console.log('Original Position:', pos);
-}
-run().catch(console.error);
